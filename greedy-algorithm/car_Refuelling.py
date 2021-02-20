15dc8bcb6fcbc6e523492a94ad5b3508d289e7b7
Siddharth <siddharthbisht99@gmail.com>
2021-02-20 22:09:52 +0530

    changed the file hiearchy and started working on the greedy problems

---
+++ greedy-algorithm/car_Refuelling.py
@@ -0,0 +1,3 @@
+# Input: A car which can travel at most L kilometers with full tank, a source point A, a destination point B and n gas stations at distances x1 ≤ x2 ≤ x3 ≤ · · · ≤ xn in kilometers from A along the path from A to B.
+
+# Output: The minimum number of refills to get from A to B, besides refill at A.
