80b3fb2ea3850007784cbac7b58811f9ebe7e603
Siddharth <siddharthbisht99@gmail.com>
2021-06-26 23:48:19 +0530

    installed indexed pq

--- 3. Coursera - algo-on-graphs/3. Path_in_graphs_2/Computing_the_Minimum_Cost_of_a_Flight.py
+++ 3. Coursera - algo-on-graphs/3. Path_in_graphs_2/Computing_the_Minimum_Cost_of_a_Flight.py
@@ -1,2 +1,22 @@
 # 2021-06-26-23-30-08.png
 # because we know that all the weights are positives we are gonna use dijkstra algo.
+
+from queue import PriorityQueue
+
+
+def minCost():
+    # def minCost(graph, source, destination):
+    pq = PriorityQueue()
+    pq.put((4, 'Read'))
+    pq.put((2, 'Play'))
+    pq.put((5, 'Write'))
+    pq.put((1, 'Code'))
+    pq.put((3, 'Study'))
+    pq.put((0, 'Study'))
+    while not pq.empty():
+        next_item = pq.get()
+        print(next_item)
+
+
+if __name__ == '__main__':
+    minCost()
