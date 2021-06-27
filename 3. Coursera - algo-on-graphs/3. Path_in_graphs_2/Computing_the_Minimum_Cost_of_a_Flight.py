4782bd160af1a7194f7e8eda8e20b883d66b9731
Siddharth <siddharthbisht99@gmail.com>
2021-06-27 09:51:02 +0530

    done with  Computing the Minimum Cost of a Flight

--- 3. Coursera - algo-on-graphs/3. Path_in_graphs_2/Computing_the_Minimum_Cost_of_a_Flight.py
+++ 3. Coursera - algo-on-graphs/3. Path_in_graphs_2/Computing_the_Minimum_Cost_of_a_Flight.py
@@ -1,22 +1,34 @@
 # 2021-06-26-23-30-08.png
 # because we know that all the weights are positives we are gonna use dijkstra algo.
+import math
+from pqdict import PQDict  # indexed priority queue
 
-from queue import PriorityQueue
 
+def minCost(graph, source, destination):
+    distanceList = [math.inf for x in range(len(graph))]
+    distanceList[source] = 0
+    visitedList = [False for x in range(len(graph))]
+    pq = PQDict({})
+    pq[str(source)] = 0
+    while len(pq) != 0:
+        currentNode, currenDistance = pq.popitem()
+        currentNode = int(currentNode)
+        for x in graph[currentNode]:
 
-def minCost():
-    # def minCost(graph, source, destination):
-    pq = PriorityQueue()
-    pq.put((4, 'Read'))
-    pq.put((2, 'Play'))
-    pq.put((5, 'Write'))
-    pq.put((1, 'Code'))
-    pq.put((3, 'Study'))
-    pq.put((0, 'Study'))
-    while not pq.empty():
-        next_item = pq.get()
-        print(next_item)
+            if visitedList[x[0]] == False:
+                if distanceList[currentNode] + x[1] < distanceList[x[0]]:
+                    distanceList[x[0]] = distanceList[currentNode] + x[1]
+                    pq[x[1]] = distanceList[currentNode] + x[1]
+        visitedList[currentNode] = True
+        if currentNode == destination:
+            print(distanceList[currentNode])
+            return
+
+    for edge in range(len(graph)):
+        pq[str(edge)] = 0
 
 
 if __name__ == '__main__':
-    minCost()
+    minCost([[[1, 4], [2, 2]], [[2, 3], [3, 2], [4, 3]], [
+            [1, 1], [3, 4], [4, 4]], [], [[3, 1]]], 0, 4)  # 6
+    minCost([[[1, 1], [2, 5]], [[2, 2]], [], [[0, 2]]], 0, 2)  # 3
