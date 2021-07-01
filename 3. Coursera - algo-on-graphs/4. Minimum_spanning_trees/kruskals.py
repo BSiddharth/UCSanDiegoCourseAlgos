a1b2c0336cde87a5ef1c5b8af7a043073aee55b0
Siddharth <siddharthbisht99@gmail.com>
2021-07-02 00:11:17 +0530

    done with kruskals

---
+++ 3. Coursera - algo-on-graphs/4. Minimum_spanning_trees/kruskals.py
@@ -0,0 +1,48 @@
+# find in union-find of disjoint set
+def find(parentList, i):
+    if parentList[i] == i:
+        return i
+    else:
+        return find(parentList, parentList[i])
+
+
+# union in union-find of disjoint set
+def union(rankList, parentList, x, y):
+    xRoot = find(parentList, x)
+    yRoot = find(parentList, y)
+
+    if rankList[x] < rankList[y]:
+        parentList[xRoot] = yRoot
+    elif rankList[x] > rankList[y]:
+        parentList[yRoot] = xRoot
+    else:
+        parentList[yRoot] = xRoot
+        rankList[y] += 1
+
+
+def kruskals(totalVertices, graph):
+    # sorting graph according to the weight. deleting is expensive compared to heap so we will not pop just iterate.
+    graph.sort(key=lambda x: x[2])
+
+    # this is the parent list for find in union-find of disjoint set
+    parentList = [x for x in range(totalVertices)]
+    rankList = [0 for x in range(totalVertices)]
+
+    msp = []
+
+    # numOfEdgesFormed = 0
+    # while numOfEdgesFormed < totalVertices - 1:
+    for x, y, weight in graph:
+        if find(parentList, x) != find(parentList, y):
+            union(rankList, parentList, x, y)
+            # numOfEdgesFormed += 1
+            msp.append([x, y, weight])
+    totalWeight = 0
+    for x, y, weight in msp:
+        totalWeight += weight
+    print('Minimum spanning tree is', msp, 'and total weight is', totalWeight)
+
+
+if __name__ == '__main__':
+    kruskals(9, [[0, 1, 28], [0, 5, 10], [1, 6, 14], [1, 2, 16], [
+             2, 3, 12], [3, 4, 22], [3, 6, 18], [6, 4, 24], [4, 5, 25]])
